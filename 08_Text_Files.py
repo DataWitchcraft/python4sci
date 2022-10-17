e174d7f5b5f10c65b52cafb494ff0a1b19ec7074
Petr Simecek <lamparna@gmail.com>
2022-10-18 00:46:02 +0200

    initial commit

--- 08_Text_Files.py
+++ 08_Text_Files.py
@@ -3,3 +3,10 @@
 # TEXT FILES
 #
 #############################################################################
+
+# to know more about how to open files for reading or writing
+# read https://swcarpentry.github.io/python-second-language/12-file-io/
+
+with open('data/species.csv', 'r') as f:
+    data = f.read()
+print('file contains', len(data), 'bytes')
